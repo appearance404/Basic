60060981aff578a87c1091aec7bfba38af606e1a
Anon <50171587+appearance404@users.noreply.github.com>
2024-07-07 01:27:19 +0600

    Update self_destruct().py

--- self_destruct().py
+++ self_destruct().py
@@ -4,3 +4,14 @@ import os
 def self_destruct():
     os.remove(__file__)
 
+import os
+
+def self_destruct():
+    try:
+        os.remove(__file__)
+        print("File successfully deleted.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    self_destruct()
